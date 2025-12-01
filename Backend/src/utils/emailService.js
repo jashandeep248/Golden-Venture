922fecb361096b17e5b067b30415e59ce339665e
jashandeep248 <138927762+jashandeep248@users.noreply.github.com>
2025-12-01 18:40:12 +0530

    Fixed bugs

--- Backend/src/utils/emailService.js
+++ Backend/src/utils/emailService.js
@@ -3,12 +3,16 @@ const nodemailer = require('nodemailer');
 class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
       secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
+      },
+      tls: {
+        rejectUnauthorized: false
       }
     });
   }
